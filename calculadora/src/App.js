976e79c2936230fd5188a1153faf7ad3683e393e
Hudson Retonde <hudsonsretonde@gmail.com>
2022-12-01 16:39:25 -0300

    Calculadora Básica de Números Inteiros Finalizada.

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -7,53 +7,83 @@ import {useState} from 'react';
 const App = () => {
   const [currentNumber, setCurrentNumber] = useState('0');
   const [firstNumebr, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('0');
 
-  const hundleOnClear = () => {
-    setCurrentNumber('0')
-    setFirstNumber('0')
+  const handleOnClear = () => {
+    setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('');
   }
 
-  const hundleAddNumber = (num) => {
+  const handleAddNumber = (num) => {
     setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
   }
 
-  const hundleSumNumbers = () => {
+  const handleSumNumbers = () => {
     if (firstNumebr === '0') {
       setFirstNumber(String(currentNumber));
       setCurrentNumber('0');
+      setOperation('+');
     } else {
       const sum = Number(firstNumebr) + Number(currentNumber);
       setCurrentNumber(String(sum));
+      setOperation('');
     }
   }
 
-  const hundleSubNumbers = () => {
+  const handleSubNumbers = () => {
     if (firstNumebr === '0') {
       setFirstNumber(String(currentNumber));
       setCurrentNumber('0');
+      setOperation('-');
     } else {
-      const sum = Number(firstNumebr) - Number(currentNumber);
-      setCurrentNumber(String(sum));
+      const sub = Number(firstNumebr) - Number(currentNumber);
+      setCurrentNumber(String(sub));
+      setOperation('-');
     }
   }
 
-  const hundleMultNumbers = () => {
+  const handleMultNumbers = () => {
     if (firstNumebr === '0') {
       setFirstNumber(String(currentNumber));
       setCurrentNumber('0');
+      setOperation('x');
     } else {
-      const sum = Number(firstNumebr) * Number(currentNumber);
-      setCurrentNumber(String(sum));
+      const mult = Number(firstNumebr) * Number(currentNumber);
+      setCurrentNumber(String(mult));
+      setOperation('x');
     }
   }
 
-  const hundleDivtNumbers = () => {
+  const handleDivNumbers = () => {
     if (firstNumebr === '0') {
       setFirstNumber(String(currentNumber));
       setCurrentNumber('0');
+      setOperation('/');
     } else {
-      const sum = Number(firstNumebr) / Number(currentNumber);
-      setCurrentNumber(String(sum));
+      const div = Number(firstNumebr) / Number(currentNumber);
+      setCurrentNumber(String(div));
+      setOperation('/');
+    }
+  }
+
+  const handleEquals = () => {
+    if(firstNumebr !== '0' && operation !== '' && currentNumber !== '0') {
+      switch(operation){
+        case '+':
+          handleSumNumbers();
+          break; 
+        case '-':
+          handleSubNumbers();
+          break;
+        case 'x':
+          handleMultNumbers();
+          break;
+        case '/':
+          handleDivNumbers();
+          break;
+        default: console.log("default");
+      }
     }
   }
 
@@ -65,28 +95,28 @@ const App = () => {
         <Input value={currentNumber}/>
 
         <Row>
-          <Button label="x" onClick={hundleMultNumbers}/>
-          <Button label="/" onClick={hundleDivtNumbers}/>
-          <Button label="c" onClick={hundleOnClear}/>
-          <Button label="#" onClick={() => hundleAddNumber('#')}/>
+          <Button label="x" onClick={handleMultNumbers}/>
+          <Button label="/" onClick={handleDivNumbers}/>
+          <Button label="c" onClick={handleOnClear}/>          
+          <Button label="=" onClick={handleEquals}/>          
         </Row>
         <Row>
-          <Button label="7" onClick={() => hundleAddNumber('7')}/>
-          <Button label="8" onClick={() => hundleAddNumber('8')}/>
-          <Button label="9" onClick={() => hundleAddNumber('9')}/>
-          <Button label="-" onClick={hundleSubNumbers}/>
+          <Button label="7" onClick={() => handleAddNumber('7')}/>
+          <Button label="8" onClick={() => handleAddNumber('8')}/>
+          <Button label="9" onClick={() => handleAddNumber('9')}/>
+          <Button label="-" onClick={handleSubNumbers}/>
         </Row>
         <Row>
-          <Button label="4" onClick={() => hundleAddNumber('4')}/>
-          <Button label="5" onClick={() => hundleAddNumber('5')}/>
-          <Button label="6" onClick={() => hundleAddNumber('6')}/>
-          <Button label="+" onClick={hundleSumNumbers}/>
+          <Button label="4" onClick={() => handleAddNumber('4')}/>
+          <Button label="5" onClick={() => handleAddNumber('5')}/>
+          <Button label="6" onClick={() => handleAddNumber('6')}/>
+          <Button label="+" onClick={handleSumNumbers}/>
         </Row>
         <Row>
-          <Button label="1" onClick={() => hundleAddNumber('1')}/>
-          <Button label="2" onClick={() => hundleAddNumber('2')}/>
-          <Button label="3" onClick={() => hundleAddNumber('3')}/>
-          <Button label="=" onClick={() => hundleAddNumber('=')}/>
+          <Button label="1" onClick={() => handleAddNumber('1')}/>
+          <Button label="2" onClick={() => handleAddNumber('2')}/>
+          <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="0" onClick={() => handleAddNumber('0')}/>
         </Row>       
       
       </Content>      
